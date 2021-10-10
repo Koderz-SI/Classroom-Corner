8d8fb66a5536c7d597352a607c2080370be7da87
SanskritiKushik5 <sanskritikushik23@gmail.com>
2021-10-10 20:31:07 +0530

    Quiz APIs student

--- controllers/quizRoutes.js
+++ controllers/quizRoutes.js
@@ -30,53 +30,54 @@ router.get("/student/quizzes", checkAuth, (req, res) => {
 		if (data) {
 			quizzes = data;
 		}
-		res.render("quiz_list", { data: quizzes });
+		res.render("studentQuiz", { data: quizzes });
 	});
 });
 
 router.get("/student/quiz/:topic", checkAuth, (req, res) => {
 	const topic = req.params.topic;
 	var questions;
-	question.find({"topic": topic}, (err, data) => {
+	question.find({ idcheck: topic }, (err, data) => {
 		if (err) {
 			console.log(err);
 		}
 		if (data) {
 			questions = data;
 		}
-		res.render("quiz", { data: questions });
+		res.render("stuTest", { data: questions, topic: topic });
 	});
 });
-
+let questions;
 router.post("/student/quiz/:topic/done", checkAuth, (req, res) => {
 	const student = req.user.name;
 	const email = req.user.email;
 	const topic = req.params.topic;
-	var questions;
-	question.find({"topic": topic}, (err, data) => {
+
+	question.find({ idcheck: topic }, (err, data) => {
 		if (err) {
 			console.log(err);
 		}
 		if (data) {
 			questions = data;
 		}
-	});
-	var correctans = 0;
-	for(var q of questions){
-		if(q.correct == req.body.ques){
-			correctans += 1;
+		var correctans = 0;
+		for (let i = 0; i < questions.length; i++) {
+			if (questions[i].correct == req.body[`${i}`]) {
+				correctans += 1;
+			}
 		}
-	}
-	var wrongans = questions.length - marks;
-	const Resultnew = new result({
-        student,
-        topic,
-        correctans,
-        wrongans
-    });
-	Resultnew.save().then(() => {
-		res.render("quiz-end", {questions: questions, correctans: correctans, wrongans: wrongans});
-	})
-	.catch((err) => console.log(err));
+		var wrongans = questions.length - correctans;
+		const Resultnew = new result({
+			student,
+			topic,
+			correctans,
+			wrongans
+		});
+		Resultnew.save().then(() => {
+			res.render("testSubmit", { questions: questions, correctans: correctans, wrongans: wrongans });
+		})
+			.catch((err) => console.log(err));
+	});
+
 })
 module.exports = router;
